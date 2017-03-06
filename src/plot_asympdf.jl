080795da7fe2c96fe07113d7a27fe35ae65d31f3
Nachikethas <nachikethas@users.noreply.github.com>
2017-03-05 17:30:15 -0800

    Updated plot

--- src/plot_asympdf.jl
+++ src/plot_asympdf.jl
@@ -6,11 +6,11 @@ ttf = "Consolas"
 
 function plot_asympdf(x)
   if x == "asympdf"
-    xest = [2/15 0 2-sqrt(2) 1/4]'
+    xest = [2/15 0 2-sqrt(3.5) 1/4]'
     est = [
       (xest[1] + res/1.5, asympdf(xest[1]), text("MMSE", font(sff, annfs))),
       (xest[2] + res/1.5, asympdf(xest[2]), text("MAP", font(sff, annfs))),
-      (xest[3] + res/1.5, asympdf(xest[3]), text("MEDE", font(sff, annfs))),
+      (xest[3] + res/0.70, asympdf(xest[3]), text("MEDE", font(sff, annfs))),
 #      (1/12, asympdf(1/12), "MP(0.5)"),
 #      (1/6, asympdf(1/6), "MP(1)"),
       (xest[4] + res/1.5, asympdf(xest[4]), text("MP(1.5)", font(sff, annfs))),
@@ -25,11 +25,14 @@ function plot_asympdf(x)
       legendfont = font(sff, tickfs),
       tickfont = font(sff, tickfs),
 
-      line = (:black, 0.5, 3, :solid),
-      xticks = -1:res:1,
+      #line = (:black, 0.5, 3, :solid),
+      linecolor = :black,
+      linewidth = 4,
+#      grid = false,
+#      xticks = -1:res:1,
       xlims = (-1, 1),
       xlabel = "Possible locations",
-      yticks = 0.1:0.1:0.9,
+#      yticks = 0.1:0.1:0.9,
       ylims = (0, 0.9),
       ylabel = "Posterior PDF",
 
@@ -55,4 +58,6 @@ function plot_asympdf(x)
   else
     error("Invalid argument")
   end
+
+  return plt
 end
